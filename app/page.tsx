fca25de74e81fb6722f6a66143371edd47522828
Emmanuel A. <alabiemmanuel2937@gmail.com>
2025-08-05 10:37:51 +0000

    edit homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,33 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 
 export default function InvitePage() {
   const clientId = process.env.NEXT_PUBLIC_DISCORD_APPLICATION_ID
   const permissions = 8 // Admin
-  // const inviteUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=${permissions}&scope=bot%20applications.commands`
-  const isDevelopment = !(window.location.hostname == 'nextjs-discordbot-template.vercel.app')
+  const inviteUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=${permissions}&scope=bot%20applications.commands`
+  
+  // Test to see whether you're actually working on the bot or just viewing the template's homepage
+  // You can remove this when you don't need it anymore
+
+  const [isDevelopment, setIsDevelopment] = useState<boolean | null>(null)
+  
+  useEffect(() => {
+    const prodDomain = 'nextjs-discordbot-template.vercel.app'
+    const currentDomain = window.location.hostname
+    setIsDevelopment(currentDomain !== prodDomain)
+  }, [])
+
+  if (isDevelopment === null) return null
+
+  // End of test
 
   return (
     <>
       {isDevelopment ? (
-        <main> Application is under development </main>
+        <main>Application is under development</main>
       ) : (
-        <main> Veiwing documentation site </main>
+        <main>Viewing documentation site</main>
       )}
     </>
   )
